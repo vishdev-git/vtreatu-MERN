a08620c41ca5d04aba065cf061f8cac5ac8b52be
Viswanath R <viswa20.2001@gmail.com>
2024-10-23 11:10:45 +0530

    Appointment booking component built

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,11 +1,98 @@
-
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const Appointment = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { docId } = useParams();
+  const { doctors, currencySymbol } = useContext(AppContext);
+  const [docInfo, setDocInfo] = useState(null);
+  const [docSlots, setDocSlots] = useState([]);
+  const [slotIndex, setSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState("");
 
-export default Appointment
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+  };
+
+  const getAvailableSlots = async ()=>{
+    setDocSlots([])
+    //Getting current date
+    let today = new Date()
+    for(let i = 0; i<7; i++){
+      //Getting date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate()+i)
+
+      // Setting end time of the date with index
+      let endTime = new Date()
+      endTime.setDate(today.getDate()+1)
+      endTime.setHours(21,0,0,0)
+
+      // Setting hours
+      if(today.getDate()=== currentDate.getDate()){
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
+  useEffect(()=>{
+    getAvailableSlots();
+  },[docInfo])
+
+  return (
+    docInfo && (
+      <div>
+        {/* Doctor details */}
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div>
+            <img
+              className="bg-primary w-full sm:max-w-72 rounded-lg"
+              src={docInfo.image}
+              alt=""
+            />
+          </div>
+
+          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
+            {/* Doc info : name, degree, experience */}
+            <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name}{" "}
+              <img className="w-5" src={assets.verified_icon} alt="" />
+            </p>
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+              <p>
+                {docInfo.degree} - {docInfo.speciality}
+              </p>
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {docInfo.experience}
+              </button>
+            </div>
+            {/* Doctor about */}
+            <div>
+              <p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
+                About <img src={assets.info_icon} alt="" />
+              </p>
+              <p className="text-sm text-gray-500 max-w[700px] mt-1">
+                {docInfo.about}
+              </p>
+            </div>
+            <p className="text-gray-500 font-medium mt-4">
+              Appointment fee :{" "}
+              <span className="text-gray-600">
+                {currencySymbol}
+                {docInfo.fees}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default Appointment;
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -18,20 +18,22 @@ const Doctors = () => {
 
   useEffect(() => {
     applyFilter();
+    console.log("Doctors:", doctors); // Check the full list of doctors
+    console.log("Filtered Doctors:", filterDoc); // Check what gets filtered
   }, [doctors, speciality]);
 
   return (
     <div>
       <p className="text-gray-600">Browse through the doctors specialist.</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
-        <div>
-          <p>General Physician</p>
-          <p>Gynecologist</p>
-          <p>Dermatologist</p>
-          <p>Pediatricians</p>
-          <p>Neurologist</p>
-          <p>Gastroenterologist</p>
-        </div>
+        <div className="flex flex-col gap-4 text-sm tex-gray-600">
+          <p onClick={()=> speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'General physician' ? "bg-indigo-100 text-black ": ""} `}>General physician</p>
+          <p onClick={()=> speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gynecologist' ? "bg-indigo-100 text-black ": ""} `}>Gynecologist</p>
+          <p onClick={()=> speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Dermatologist' ? "bg-indigo-100 text-black ": ""} `}>Dermatologist</p>
+          <p onClick={()=> speciality === 'Pediatricians' ? navigate('/doctors') : navigate('/doctors/Pediatricians')} className={`w-[94] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Pediatricians' ? "bg-indigo-100 text-black ": ""} `}>Pediatricians</p>
+          <p onClick={()=> speciality === 'Neurologist' ? navigate('/doctors') : navigate('/doctors/Neurologist')} className={`w-[94] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Neurologist' ? "bg-indigo-100 text-black ": ""} `}>Neurologist</p>
+          <p onClick={() => speciality === 'Gastroenterologist' ? navigate('/doctors') : navigate('/doctors/Gastroenterologist')} className={`w-[94] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gastroenterologist' ? "bg-indigo-100 text-black ": ""} `}>Gastroenterologist</p>
+         </div>
         <div className="w-full grid grid-cols-auto gap-4">
           {filterDoc.map((item, index) => (
             <div
